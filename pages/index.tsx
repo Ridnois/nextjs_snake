67c9f6d5da95f25b2dec7ebedf379cf35caa2772
Sebastian Altamirano <ridnois@protonmail.com>
2022-03-18 15:13:46 -0300

    cleaner

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,31 +23,29 @@ type IGrid = IRow[]
 export const Grid = (size: number): IGrid => {
   const grid: IGrid = []
   for (let i = 0; i < size; i++) {
-    const row: IRow = [] 
-    for(let j = 0; j < size; j++) {
+    const row: IRow = []
+    for (let j = 0; j < size; j++) {
       row[j] = {
         status: 'empty',
         y: i,
         x: j,
       }
     }
-    grid[i] = row;
+    grid[i] = row
   }
-  return grid;
+  return grid
 }
 
-export const CellBox: React.FC<ICell> = ({status}) => {
-  return (
-    <div className={`${styles.cell} ${styles[`cell--${status}`]}`}></div>
-  )
+export const CellBox: React.FC<ICell> = ({ status }) => {
+  return <div className={`${styles.cell} ${styles[`cell--${status}`]}`}></div>
 }
 
-export const CellRow: React.FC<{row: IRow}> = ({row}) => {
+export const CellRow: React.FC<{ row: IRow }> = ({ row }) => {
   return (
     <div className={styles['snake__row']}>
-      {
-        row.map((cell: ICell) => (<CellBox key={`${cell.x}${cell.y}`} {...cell}/>))
-      }
+      {row.map((cell: ICell) => (
+        <CellBox key={`${cell.x}${cell.y}`} {...cell} />
+      ))}
     </div>
   )
 }
@@ -56,10 +54,10 @@ export const drawPoint = (grid: IGrid, cell: ICell) => {
   return grid.map((row, index) => {
     if (index === cell.y) {
       row.map((c, index) => {
-        if(index === cell.x) {
-          c.status = cell.status;
+        if (index === cell.x) {
+          c.status = cell.status
         }
-        return c;
+        return c
       })
     }
     return row
@@ -70,7 +68,7 @@ export const putRandomFood = (grid: IGrid) => {
   const r1 = Math.floor(Math.random() * grid.length)
   const r2 = Math.floor(Math.random() * grid.length)
   const newGrid = [...grid]
-  if(grid[r1][r2].status !== 'snake') {
+  if (grid[r1][r2].status !== 'snake') {
     newGrid[r1][r2].status = 'food'
   } else {
     putRandomFood(grid)
@@ -79,52 +77,53 @@ export const putRandomFood = (grid: IGrid) => {
   return newGrid
 }
 
-
 export const cleanSnakeTrack = (grid: IGrid) => {
-  return grid.map(row => row.map(cell => {
-    if (cell.status === 'snake') {
-      cell.status = 'empty';
-    }
-    return cell;
-  }))
+  return grid.map((row) =>
+    row.map((cell) => {
+      if (cell.status === 'snake') {
+        cell.status = 'empty'
+      }
+      return cell
+    })
+  )
 }
 // Move body of the snake
 export const moveSnake = (snake: ISnake, mustGrow = false) => {
   let head = snake.body[0]
-  let newHead = {status: 'snake', ...head};
-  switch(snake.direction) {
+  let newHead = { status: 'snake', ...head }
+  switch (snake.direction) {
     case 'up':
       newHead.y = head.y - 1
-      break;
+      break
     case 'down':
       newHead.y = head.y + 1
-      break;
+      break
     case 'right':
       newHead.x = head.x + 1
-      break;
+      break
     case 'left':
       newHead.x = head.x - 1
   }
-  console.log([newHead, ...snake.body]) 
-  return {...snake, body: [newHead, ...snake.body]}
+  console.log([newHead, ...snake.body])
+  return { ...snake, body: [newHead, ...snake.body] }
 }
 
 export const Snake: NextPage = () => {
-  const [grid, setGrid] = useState(Grid(32));
+  const [grid, setGrid] = useState(Grid(32))
   const [snake, setSnake] = useState<ISnake>({
-      direction: 'left',
-      body: [
-        {x: 16, y: 16},
-        {x: 17, y: 16},
-        {x: 18, y: 16},
-      ]
-    })
+    direction: 'left',
+    body: [
+      { x: 16, y: 16 },
+      { x: 17, y: 16 },
+      { x: 18, y: 16 },
+    ],
+  })
   const move = () => {
-    setSnake(snake => moveSnake(snake))
+    setSnake((snake) => moveSnake(snake))
   }
   const setDirection = (direction: 'right' | 'left' | 'up' | 'down') => {
     console.log(direction)
-    setSnake((snake: ISnake) => ({...snake, direction} as any))
+    setSnake((snake: ISnake) => ({ ...snake, direction } as any))
   }
   useEffect(() => {
     putRandomFood(grid)
@@ -132,7 +131,7 @@ export const Snake: NextPage = () => {
   useEffect(() => {
     cleanSnakeTrack(grid)
     snake.body.map((cell) => {
-      setGrid((grid) => drawPoint(grid, {status: 'snake', ...cell}))
+      setGrid((grid) => drawPoint(grid, { status: 'snake', ...cell }))
     })
   }, [snake])
   return (
@@ -147,9 +146,9 @@ export const Snake: NextPage = () => {
       <button onClick={() => setDirection('left')}>Left</button>
       <button onClick={() => setDirection('right')}>Right</button>
       <div className={styles['snake__container']}>
-        {
-          grid.map((row, index) => (<CellRow key={index} row={row}/>))
-        }
+        {grid.map((row, index) => (
+          <CellRow key={index} row={row} />
+        ))}
       </div>
     </div>
   )
